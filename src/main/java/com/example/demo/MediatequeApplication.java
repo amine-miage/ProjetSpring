ef30f64160d08df1e5877472d7bc0300b6ffd463
zakaria HAIMI <zakaria.haimi@uha.fr>
2017-12-18 23:58:39 +0100

    init accueil

--- src/main/java/com/example/demo/MediatequeApplication.java
+++ src/main/java/com/example/demo/MediatequeApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.example.dao.UserRepository;
@@ -15,6 +16,7 @@ import com.example.entity.User;
 
 @EntityScan("com.example.entity")
 @EnableJpaRepositories("com.example.dao")
+@ComponentScan("com.example.service")
 @SpringBootApplication
 public class MediatequeApplication {
 

b4524984431a275d63f4d3b085d1841bb2e9ed69
zakaria HAIMI <zakaria.haimi@uha.fr>
2017-12-19 20:03:29 +0100

    page accueil et login

--- src/main/java/com/example/service/AccueilController.java
+++ src/main/java/com/example/service/AccueilController.java
@@ -11,4 +11,10 @@ public class AccueilController {
         
         return "accueil";
     }
+    
+    @RequestMapping("/login")
+    public String login() {
+        
+        return "login";
+    }
 }

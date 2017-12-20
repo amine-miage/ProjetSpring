847e1a84b4a410852da9ef560b47ad4e17dd7639
zakaria HAIMI <zakaria.haimi@uha.fr>
2017-12-20 23:27:29 +0100

    partie Admin done

--- src/main/java/com/example/dao/UserRepository.java
+++ src/main/java/com/example/dao/UserRepository.java
@@ -17,7 +17,9 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     User findById(int id);
     User findByMailAndPassword(String mail, String password);
     List<User> findByRole(String r);
-    @Query("delete from User u where id = :id ")
-    void deleteUserById(@Param("id") int id);
+    
+    void deleteById( int id);
+    @Query("select u from User u where u.name like :x AND u.role='Employe'")
+    List<User> chercher(@Param("x") String mc);
 
 }
--- src/main/java/com/example/service/AdminController.java
+++ src/main/java/com/example/service/AdminController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,22 +26,32 @@ public class AdminController {
 	UserRepository ur;
 	
 	@RequestMapping(value="/gestionEmploye",method=RequestMethod.GET)
-	public String gererEmployer(Model model){
+	public String gererEmployer(Model model ,
+			@RequestParam(name="motCle" , defaultValue="" )String mc){
 		
-		List<User> users = ur.findByRole("Employe");
+		List<User> users = ur.chercher("%"+mc+"%");
 		model.addAttribute("users",users);
 		return "gestionEmploye";
 	}
 	
 	@RequestMapping(value="/formAdmin",method=RequestMethod.GET)
-	public String formAdmin(Model model){
-		List<User> users = ur.findByRole("Employe");
+	public String formAdmin(Model model ,
+			@RequestParam(name="motCle" , defaultValue="" )String mc){
+		
+		List<User> users = ur.chercher("%"+mc+"%");
 		model.addAttribute("users",users);
 		model.addAttribute("user",new User());
 		
 		return "formAdmin";
 	}
-	
+	@RequestMapping(value="/edit",method=RequestMethod.GET)
+	public String edit(Model model , @RequestParam(value="userId")int id){
+		User  u = ur.findById(id);
+		
+		model.addAttribute("u",u);
+		
+		return "EditForm";
+	}
 	@RequestMapping(value="/SaveEmploye",method=RequestMethod.POST)
 	public String saveEmploye(@Valid User empl , BindingResult br, Model model){
 	
@@ -59,10 +70,11 @@ public class AdminController {
 		return "redirect:/Admin/gestionEmploye";
 	}
 	
+	@Transactional
 	@RequestMapping(value="/DeleteEmploye", method=RequestMethod.GET)
 	public String deleteEmploye(@RequestParam(value="userId") int id){
 	
-		ur.deleteUserById(id);
+		ur.deleteById(id);
 		
 		return "redirect:/Admin/gestionEmploye";
 	}	
